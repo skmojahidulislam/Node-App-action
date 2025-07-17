4428ab041ce8dbe551b35eade393ad56423b8ed7
skmojahidulislam <skmojahidulislam298@gmail.com>
2025-07-17 20:15:36 +0530

    node-files

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.json(
+        [
+            {
+                id: 1,
+                name: "Mujhaid",
+                salary: 60000
+            },
+
+            {
+                id: 2,
+                name: "Huzaifa",
+                salary: 70000
+            },
+
+            {
+                id: 3,
+                name: "Abdul",
+                salary: 80000
+            },
+
+            {
+                id: 4,
+                name: "Salim",
+                salary: 90000
+            },
+
+            {
+                id: 5,
+                name: "Abdul",
+                salary: 95000
+            },
+        ]
+    )
+});
+
+app.listen(port, () => {
+    console.log("This app runn port no: 5000")
+})
